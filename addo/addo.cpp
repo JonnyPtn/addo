499bd86ae4903a24ec2d61a68364c2e9602fa5bd
jonathan.r.paton@googlemail.com <jonathan.r.paton@googlemail.com>
2017-04-15 11:37:53 +0100

    Reduced it to once executable. Seems massively dangerous. I like it

--- addo/addo.cpp
+++ addo/addo.cpp
@@ -5,27 +5,109 @@
 ///									
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#include <string>
-#include <iostream>
-
 #include <Windows.h>
+#include <string>
+#include <array>
+#include <memory>
+#include <stdio.h>
+#include <iostream>
+#include <codecvt>
 
 #define BUFSIZE 128 
 
-int main(int argc, char* argv[])
+std::wstring myPath() {
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileName(NULL, buffer, MAX_PATH);
+	return std::wstring(buffer);
+}
+
+
+int runpiped(int argc, wchar_t* argv[])
 {
-	//parse input
-	std::string command;
-	for (int i = 1; i < argc; i++)
+	//parse the command first
+	std::wstring command = L"cmd /c";
+	for (int i = 2; i < argc; i++) // magic 2 because we ignore "-piped" parameter
 	{
-		std::string arg(argv[i]);
-		if (arg.find(" ") != std::string::npos)
+		std::wstring arg(argv[i]);
+		if (arg.find(L" ") != std::wstring::npos)
 		{
+			//parameter has white space, wrap in speech marks
 			arg.insert(arg.begin(), '"');
-			arg.append("\"");
+			arg.append(L"\"");
 		}
 		command.append(arg);
-		command.append(" ");
+		command.append(L" ");
+	}
+
+	// Open the named pipe
+	// Most of these parameters aren't very relevant for pipes.
+	HANDLE hpipe;
+	do
+	{
+		hpipe = CreateFile(
+			L"\\\\.\\pipe\\sudopipe",
+			GENERIC_WRITE, // only need write access
+			FILE_SHARE_READ | FILE_SHARE_WRITE,
+			NULL,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			NULL
+		);
+
+	} while (hpipe == INVALID_HANDLE_VALUE);
+
+	DWORD dwWritten;
+
+	// I hate this
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+	std::string converted_str = converter.to_bytes(command);
+
+	std::array<char, 128> buffer;
+	std::shared_ptr<FILE> pipe(_popen(converted_str.c_str(), "r"), _pclose);
+	if (!pipe) throw std::runtime_error("popen() failed!");
+	while (!feof(pipe.get()))
+	{
+		if (fgets(buffer.data(), 128, pipe.get()) != NULL)
+		{
+			std::cout << buffer.data();
+			auto  wresult = WriteFile(
+				hpipe, // handle to our outbound pipe
+				buffer.data(), // data to send
+				DWORD(buffer.size()), // length of data to send (bytes)
+				&dwWritten, // will store actual amount of data sent
+				NULL // not using overlapped IO
+			);
+		}
+	}
+	return 0;
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+	//parse input
+	std::wstring command;
+
+	//If the first parameter is -piped, then we should run piped
+	if (std::wstring(argv[1]) == L"-piped")
+	{
+		return runpiped(argc, argv);
+	}
+	//otherwise, we are the pipee
+	else
+	{
+		command.append(L"-piped ");
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		std::wstring arg(argv[i]);
+		if (arg.find(L" ") != std::wstring::npos)
+		{
+			arg.insert(arg.begin(), '"');
+			arg.append(L"\"");
+		}
+		command.append(arg);
+		command.append(L" ");
 	}
 
 	//create a named pipe. The shell command we execute with elevated priviledges will write to this
@@ -39,6 +121,7 @@ int main(int argc, char* argv[])
 		NULL
 	);
 
+	auto path = myPath();
 
 	//shell execute with elevated permissions
 	SHELLEXECUTEINFO shExInfo = { 0 };
@@ -46,8 +129,8 @@ int main(int argc, char* argv[])
 	shExInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
 	shExInfo.hwnd = 0;
 	shExInfo.lpVerb = L"runas";                // Operation to perform
-	shExInfo.lpFile = L"C:/sudo/pipedcmd.exe";       // Application to start
-	shExInfo.lpParameters = LPCWSTR(command.c_str());                  // Additional parameters
+	shExInfo.lpFile = path.c_str();       // Application to start
+	shExInfo.lpParameters = command.c_str();                  // Additional parameters
 	shExInfo.lpDirectory = 0;
 	shExInfo.nShow = SW_HIDE;
 	shExInfo.hInstApp = 0;
